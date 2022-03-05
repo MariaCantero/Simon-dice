2cb1b7fc67fc30c113d51e2af3f7297afb798e5f
Maria Cantero <marifer120701@gmail.com>
2022-03-05 15:16:44 -0300

    js

--- js/main.js
+++ js/main.js
@@ -1,138 +1,133 @@
-const celeste = document.getElementById('celeste')
-const violeta = document.getElementById('violeta')
-const naranja = document.getElementById('naranja')
-const verde = document.getElementById('verde')
-const btnEmpezar = document.getElementById('btnEmpezar')
-const ULTIMO_NIVEL = 5
+const round = document.getElementById('round');
+const simonButtons = document.getElementsByClassName('square');
+const startButton = document.getElementById('startButton');
 
-
-class Juego {
-  constructor() {
-    this.inicializar = this.inicializar.bind(this)
-    this.inicializar()
-    this.generarSecuencia()
-    setTimeout(this.siguienteNivel, 500) 
-  }
-
-  inicializar() {
-    this.siguienteNivel = this.siguienteNivel.bind(this)
-    this.elegirColor = this.elegirColor.bind(this)
-    this.toggleBtnEmpezar()
-    this.nivel = 1
-    this.colores = {
-        celeste,
-        violeta,
-        naranja,
-        verde
+class Simon {
+    constructor(simonButtons, startButton, round) {
+        this.round = 0;
+        this.userPosition = 0;
+        this.totalRounds = 10;
+        this.sequence = [];
+        this.speed = 1000;
+        this.blockedButtons = true;
+        this.buttons = Array.from(simonButtons);
+        this.display = {
+            startButton,
+            round
+        }
+        this.errorSound = new Audio('./sounds/error.wav');
+        this.buttonSounds = [
+            new Audio('./sounds/1.mp3'),
+            new Audio('./sounds/2.mp3'),
+            new Audio('./sounds/3.mp3'),
+            new Audio('./sounds/4.mp3'),
+        ]
     }
-  }
-  toggleBtnEmpezar() {
-      if (btnEmpezar.classList.contains('hide')) {
-        btnEmpezar.classList.remove('hide')
-      } else {
-        btnEmpezar.classList.add('hide')
-      }
-  }
 
-  generarSecuencia() {
-      this.secuencia =  new Array(ULTIMO_NIVEL).fill(0).map( n => Math.floor(Math.random() * 4))
-  }
+    // Inicia el Simon
+    init() {
+        this.display.startButton.onclick = () => this.startGame();
+    }
 
-  siguienteNivel() {
-      this.subnivel = 0 
-      this.iluminarSecuencia()
-      this.agregarEventosClick()
-  }
+    // Comienza el juego
+    startGame() {
+        this.display.startButton.disabled = true; 
+        this.updateRound(0);
+        this.userPosition = 0;
+        this.sequence = this.createSequence();
+        this.buttons.forEach((element, i) => {
+            element.classList.remove('winner');
+            element.onclick = () => this.buttonClick(i);
+        });
+        this.showSequence();
+    }
 
-  transformarNumeroaColor(numero) {
-      switch (numero) {
-          case 0:
-              return 'celeste'
-          case 1:
-              return 'violeta'
-          case 2:
-              return 'naranja'
-          case 3:
-              return 'verde'        
-      }
-  }
+    // Actualiza la ronda y el tablero
+    updateRound(value) {
+        this.round = value;
+        this.display.round.textContent = `Round ${this.round}`;
+    }
 
-  transformarColorANumero(numero) {
-    switch (numero) {
-        case 'celeste':
-            return 0
-        case 'violeta':
-            return 1
-        case 'naranja':
-            return 2
-        case 'verde':
-            return 3       
+    // Crea el array aleatorio de botones
+    createSequence() {
+        return Array.from({length: this.totalRounds}, () =>  this.getRandomColor());
+    }
+
+    // Devuelve un número al azar entre 0 y 3
+    getRandomColor() {
+        return Math.floor(Math.random() * 4);
+    }
+
+    // Ejecuta una función cuando se hace click en un botón
+    buttonClick(value) {
+        !this.blockedButtons && this.validateChosenColor(value);
+    }
+
+    // Valida si el boton que toca el usuario corresponde a al valor de la secuencia
+    validateChosenColor(value) {
+        if(this.sequence[this.userPosition] === value) {
+            this.buttonSounds[value].play();
+            if(this.round === this.userPosition) {
+                this.updateRound(this.round + 1);
+                this.speed /= 1.02;
+                this.isGameOver();
+            } else {
+                this.userPosition++;
+            }
+        } else {
+            this.gameLost();
+        }
+    }
+
+    // Verifica que no haya acabado el juego
+    isGameOver() {
+        if (this.round === this.totalRounds) {
+            this.gameWon();
+        } else {
+            this.userPosition = 0;
+            this.showSequence();
+        };
+    }
+
+    // Muestra la secuencia de botones que va a tener que tocar el usuario
+    showSequence() {
+        this.blockedButtons = true;
+        let sequenceIndex = 0;
+        let timer = setInterval(() => {
+            const button = this.buttons[this.sequence[sequenceIndex]];
+            this.buttonSounds[this.sequence[sequenceIndex]].play();
+            this.toggleButtonStyle(button)
+            setTimeout( () => this.toggleButtonStyle(button), this.speed / 2)
+            sequenceIndex++;
+            if (sequenceIndex > this.round) {
+                this.blockedButtons = false;
+                clearInterval(timer);
+            }
+        }, this.speed);
+    }
+
+    // Pinta los botones para cuando se está mostrando la secuencia
+    toggleButtonStyle(button) {
+        button.classList.toggle('active');
+    }
+
+    // Actualiza el simon cuando el jugador pierde
+    gameLost() {
+        this.errorSound.play();
+        this.display.startButton.disabled = false; 
+        this.blockedButtons = true;
+    }
+
+    // Muestra la animacón de triunfo y actualiza el simon cuando el jugador gana
+    gameWon() {
+        this.display.startButton.disabled = false; 
+        this.blockedButtons = true;
+        this.buttons.forEach(element =>{
+            element.classList.add('winner');
+        });
+        this.updateRound('🏆');
     }
 }
 
-  iluminarSecuencia() {
-       for (let i = 0; i < this.nivel; i++) {
-           const color = this.transformarNumeroaColor(this.secuencia[i])
-           setTimeout (() => this.iluminarColor(color), 1000 * i) 
-       }
-  }
-
-  iluminarColor(color) {
-      this.colores[color].classList.add('light')
-      setTimeout (() => this.apagarColor(color), 350)
-  }
-
-  apagarColor(color) {
-      this.colores[color].classList.remove('light')
-  }
-
-  agregarEventosClick() {
-      this.colores.celeste.addEventListener('click', this.elegirColor)
-      this.colores.verde.addEventListener('click', this.elegirColor)
-      this.colores.violeta.addEventListener('click', this.elegirColor)
-      this.colores.naranja.addEventListener('click', this.elegirColor)
-  }
-
-  eliminarEventosClick() {
-    this.colores.celeste.removeEventListener('click', this.elegirColor)
-    this.colores.verde.removeEventListener('click', this.elegirColor)
-    this.colores.violeta.removeEventListener('click', this.elegirColor)
-    this.colores.naranja.removeEventListener('click', this.elegirColor)
-  }
-
-  elegirColor(ev) {
-      const nombreColor = ev.target.dataset.color
-      const numeroColor = this.transformarColorANumero(nombreColor)
-      this.iluminarColor(nombreColor)
-      if (numeroColor === this.secuencia[this.subnivel]) {
-          this.subnivel++
-          if(this.subnivel === this.nivel) {
-              this.nivel++
-              this.eliminarEventosClick()
-              if (this.nivel === (ULTIMO_NIVEL + 1)) {
-                  this.ganoELJuego()
-              } else {
-                  setTimeout(this.siguienteNivel, 1000)
-              }
-          }
-      } else {
-          this.perdioElJuego()
-      }
-  }
-  ganoELJuego() {
-      swal('Juego','Felicitaciones, ganaste el juego!', 'success')
-      .then(this.inicializar)
-  }
-
-  perdioElJuego() {
-    swal('Juego','Lo lamentamos, perdiste el juego!', 'error')
-    .then (()=> {
-        this.eliminarEventosClick()
-        this.inicializar()
-    })
-}
-}
-
-function empezarJuego() {
-  window.juego = new Juego()
-}
+const simon = new Simon(simonButtons, startButton, round);
+simon.init();
